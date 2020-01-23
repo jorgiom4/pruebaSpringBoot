f54cecc2db6e5e95cbb40809eea425c647c77aee
Gorka López-Quintana <gorkalqy@hotmail.com>
2020-01-23 10:11:29 +0100

    Primer commit inicial del proyecto que contiene la estructura básica general.

---
+++ src/test/java/com/nmemo/prueba/spring/PruebaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmemo.prueba.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
